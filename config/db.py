49d58ca541e2555033d120488d9e2270f2838128
Hung <hung16132016@gmail.com>
2023-11-07 07:40:29 +0700

    update connect db

--- config/db.py
+++ config/db.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine, MetaData
 
-# engine = create_engine(
-#     "mysql+pymysql://sbh:Sbh123@143.198.223.56/phpmyadmin", echo=True)
 engine = create_engine(
-    "mysql+pymysql://root@localhost:3306/sbh_apis", echo=True)
+    "mysql+pymysql://hungnn2:Hung123@143.198.223.56/phpmyadmin", echo=True)
+# engine = create_engine(
+#     "mysql+pymysql://root@localhost:3306/sbh_apis", echo=True)
 meta = MetaData()
 conn = engine.connect()
